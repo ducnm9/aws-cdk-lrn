a29687c9b2f66820ac6be89ada367adb996d7344
Nguyen Minh Duc <duc@forsharing.info>
2025-12-02 22:58:16 +0700

    Add BucketTagger aspect for tagging S3 buckets

--- bin/cdk-starter.ts
+++ bin/cdk-starter.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { PhotosStack } from '../lib/PhotosStack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Targger';
 
 const app = new cdk.App();
 // new CdkStarterStack(app, 'CdkStarter2Stack', {
@@ -26,3 +27,6 @@ const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
   targetBucketArn: photosStack.photosBucketArn,
 });
+
+const  tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger); 
